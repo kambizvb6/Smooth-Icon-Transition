556c6bdb99bfb86b0c97403e27e81b6a8f129e38
kambizvb6 <kambizvb6@gmail.com>
2023-11-13 23:58:08 -0600

    Initial Commit

---
+++ Smooth Icon Transition/Smooth_Icon_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smooth_Icon_TransitionApp.swift
+//  Smooth Icon Transition
+//
+//  Created by Kambiz on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct Smooth_Icon_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
